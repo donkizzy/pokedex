2aa1280e6a28c978473e355a89f98d4c5a366158
Amos Godwin <Amosgodwin500@gmail.com>
2021-11-16 22:11:09 +0100

    added media query height and width helpers

---
+++ pokemon_go/lib/shared/app_constant.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/material.dart';
+
+///MediaQuery Width
+double width(BuildContext context) {
+  return MediaQuery.of(context).size.width;
+}
+
+///MediaQuery Height
+double height(BuildContext context) {
+  return MediaQuery.of(context).size.height;
+}
