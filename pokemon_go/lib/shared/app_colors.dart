6a75a5d64a4218d184a080da6109f5d7fbdb87d7
Amos Godwin <Amosgodwin500@gmail.com>
2021-11-17 22:56:24 +0100

    added new colours

--- pokemon_go/lib/shared/app_colors.dart
+++ pokemon_go/lib/shared/app_colors.dart
@@ -6,3 +6,5 @@ import 'package:flutter/material.dart';
 const Color ceruleanBlue = Color(0xff3558CD);
 const Color mirageBlue = Color(0xff161A33);
 const Color doveGrey = Color(0xff6B6B6B);
+const Color mecuryGrey = Color(0xffE8E8E8);
+const Color cerisePink = Color(0xffCD2873);
