4c1be56c7e5a013cba5cadbca70f6d11580031d1
Amos Godwin <Amosgodwin500@gmail.com>
2021-11-17 22:57:52 +0100

    created a custom linear indicator

---
+++ pokemon_go/lib/ui/widgets/custom_linear_indicator.dart
@@ -0,0 +1,46 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:pokemon_go/shared/app_colors.dart';
+
+class ProgressBar extends StatelessWidget {
+
+  final double current;
+  final Color color;
+
+  const ProgressBar(
+      {Key? key,
+        required this.current,
+        this.color = cerisePink})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (_, boxConstraints) {
+        var x = boxConstraints.maxWidth;
+        var percent = (current / 100) * x;
+        return Stack(
+          children: [
+            Container(
+              width: x,
+              height: 5,
+              decoration: BoxDecoration(
+                color: mecuryGrey,
+                borderRadius: BorderRadius.circular(35),
+              ),
+            ),
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 500),
+              width: percent,
+              height: 5,
+              decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.circular(35),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
