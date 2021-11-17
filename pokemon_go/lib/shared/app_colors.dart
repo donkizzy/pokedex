f54d32fdc700c63236ce4270f956da8eb2427021
Amos Godwin <Amosgodwin500@gmail.com>
2021-11-17 23:29:22 +0100

    added a new color for the linear indicator

--- pokemon_go/lib/shared/app_colors.dart
+++ pokemon_go/lib/shared/app_colors.dart
@@ -8,3 +8,4 @@ const Color mirageBlue = Color(0xff161A33);
 const Color doveGrey = Color(0xff6B6B6B);
 const Color mecuryGrey = Color(0xffE8E8E8);
 const Color cerisePink = Color(0xffCD2873);
+const Color goldTipsYellow = Color(0xffEEC218);
