8743bf2ae0ec068c49793d7ec8d93f063876b223
Amos Godwin <Amosgodwin500@gmail.com>
2021-11-19 10:57:26 +0100

    created a Bloc for fetching pokemon

--- pokemon_go/lib/network/network_routes.dart
+++ pokemon_go/lib/network/network_routes.dart
@@ -5,6 +5,6 @@ abstract class NetworkRoutes {
 
   static const String baseUrl = 'https://pokeapi.co/api/v2/pokemon';
 
-  static String deleteMessage(int offset) => baseUrl  + '?offset=50&limit=20';
+  static String fetchPokemon(int offset) => baseUrl  + '?offset=50&limit=20';
 
 }
