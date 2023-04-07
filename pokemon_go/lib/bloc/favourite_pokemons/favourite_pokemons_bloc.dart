ebe0b6ff00a7d4402fc025b042e7d1780b277055
Amos Godwin <amosgodwin500@gmail.com>
2023-04-07 22:33:08 +0100

    fetch next pokemon

--- pokemon_go/lib/bloc/favourite_pokemons/favourite_pokemons_bloc.dart
+++ pokemon_go/lib/bloc/favourite_pokemons/favourite_pokemons_bloc.dart
@@ -1,27 +1,23 @@
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:pokemon_go/models/pokemon.dart';
 
-part 'favourite_pokemons_event.dart';
 part 'favourite_pokemons_state.dart';
 
-class FavouritePokemonsBloc extends Bloc<FavouritePokemonsEvent, FavouritePokemonsState> {
-  FavouritePokemonsBloc() : super(FavouritePokemonsInitial()) {
-    on<FavouritePokemonsEvent>((event, emit)async {
+class FavouritePokemonsBloc extends Cubit<FavouritePokemonsState> {
 
-      if (event is FetchFavouritePokemon){
+  FavouritePokemonsBloc() : super(FavouritePokemonsInitial()) ;
 
+  void fetchFavouritePokemon() async{
         try{
-
           emit(FavouritePokemonsLoading());
 
           var box = Hive.box('pokemonBox');
 
-          var allPokemon = box.values;
+          Iterable<dynamic> allPokemon = box.values;
 
-          var pokemons = allPokemon.map((e) => Pokemon.fromJson(Map<String, dynamic>.from(e)));
+          Iterable<dynamic> pokemons = allPokemon.map((e) => Pokemon.fromJson(Map<String, dynamic>.from(e)));
 
           List<Pokemon> pokemonList =  List<Pokemon>.from(pokemons) ;
 
@@ -30,9 +26,5 @@ class FavouritePokemonsBloc extends Bloc<FavouritePokemonsEvent, FavouritePokemo
         }catch(e){
           emit( FavouritePokemonsError(e.toString()));
         }
-
-      }
-
-    });
   }
 }
--- pokemon_go/lib/bloc/fetch_pokemons/fetch_pokemon_bloc.dart
+++ pokemon_go/lib/bloc/fetch_pokemons/fetch_pokemon_bloc.dart
@@ -1,65 +1,57 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pokemon_go/bloc/fetch_pokemons/fetch_pokemon_event.dart';
 import 'package:pokemon_go/bloc/fetch_pokemons/fetch_pokemon_state.dart';
 import 'package:palette_generator/palette_generator.dart';
 import 'package:pokemon_go/models/pokemon.dart';
 import 'package:pokemon_go/repository/pokemon_repository.dart';
 import 'package:pokemon_go/shared/utilities.dart';
 
-class FetchPokemonBloc extends Bloc<FetchPokemonEvent, FetchPokemonState> {
+class FetchPokemonBloc extends Cubit<FetchPokemonState> {
   final PokemonRepository _pokemonRepository = PokemonRepository();
 
-  FetchPokemonBloc() : super(FetchPokemonInitial()) {
-    on<FetchPokemonEvent>((event, emit) async {
-      if (event is FetchPokemon) {
-        await fetchPokeMon(emit);
-      }
-      if (event is FetchPokemonNextPage) {
-        try {
+  FetchPokemonBloc() : super(FetchPokemonInitial());
 
-          emit(FetchPokemonNextPageLoading());
-          var response = await _pokemonRepository.fetchPokemonNextList();
-          List<Pokemon> pokemonList = <Pokemon>[];
+  void fetchNextPokeMon() async {
+    try {
+      emit(FetchPokemonNextPageLoading());
+      var response = await _pokemonRepository.fetchPokemonNextList();
+      List<Pokemon> pokemonList = <Pokemon>[];
 
-          if (response.last.status) {
-            //loop through the list to modify the data
-            for (var e in response) {
-              //loop through the stats object and sum up the stats so that the average power can be easily gotten
-              var statArray =
-                  e.stats!.map((e) => e.baseStat).reduce((a, b) => a + b);
+      if (response.last.status) {
+        //loop through the list to modify the data
+        for (var e in response) {
+          //loop through the stats object and sum up the stats so that the average power can be easily gotten
+          var statArray =
+              e.stats!.map((e) => e.baseStat).reduce((a, b) => a + b);
 
-              //loop through the types object to get the types data and concatenate it
-              var typesArray = e.types!.map((e) => e.type.name.toCapitalize());
+          //loop through the types object to get the types data and concatenate it
+          var typesArray = e.types!.map((e) => e.type.name.toCapitalize());
 
-              // loop through to get the dominant color in an image
-              PaletteGenerator palette =
-                  await PaletteGenerator.fromImageProvider(
-                NetworkImage(e.sprites!.other.officialArtwork.frontDefault),
-              );
+          // loop through to get the dominant color in an image
+          PaletteGenerator palette = await PaletteGenerator.fromImageProvider(
+            NetworkImage(e.sprites!.other.officialArtwork.frontDefault),
+          );
 
-              //calculate the average power and bmi
-              var bmi = (e.weight! / (e.height! * e.height!)).toStringAsFixed(2);
-              var averagePower = double.parse((statArray / 6).toStringAsFixed(2));
+          //calculate the average power and bmi
+          var bmi = (e.weight! / (e.height! * e.height!)).toStringAsFixed(2);
+          var averagePower = double.parse((statArray / 6).toStringAsFixed(2));
 
-              var pokemon = Pokemon(
-                  types: typesArray.join(','),
-                  averagePower: averagePower,
-                  bgColor: palette.dominantColor!.color,
-                  pokemonDetails: e,
-                  bmi: bmi);
-              pokemonList.add(pokemon);
-            }
-
-            emit(FetchPokemonSuccessful(pokemonList));
-          } else {
-            emit(FetchPokemonError(response.last.message));
-          }
-        } catch (e) {
-          emit(FetchPokemonError(e.toString()));
+          var pokemon = Pokemon(
+              types: typesArray.join(','),
+              averagePower: averagePower,
+              bgColor: palette.dominantColor!.color,
+              pokemonDetails: e,
+              bmi: bmi);
+          pokemonList.add(pokemon);
         }
+
+        emit(FetchPokemonSuccessful(pokemonList));
+      } else {
+        emit(FetchPokemonError(response.last.message));
       }
-    });
+    } catch (e) {
+      emit(FetchPokemonError(e.toString()));
+    }
   }
 
   Future<void> fetchPokeMon(Emitter<FetchPokemonState> emit) async {
@@ -70,15 +62,14 @@ class FetchPokemonBloc extends Bloc<FetchPokemonEvent, FetchPokemonState> {
       List<Pokemon> pokemonList = <Pokemon>[];
 
       if (response.last.status) {
-
         //loop through the list to modify the data
         for (var e in response) {
-
           //loop through the stats object and sum up the stats so that the average power can be easily gotten
           int statSum = e.stats!.map((e) => e.baseStat).reduce((a, b) => a + b);
 
           //loop through the types object to get the types data and concatenate it
-          List<String> typesList = e.types!.map((e) => e.type.name.toCapitalize()).toList();
+          List<String> typesList =
+              e.types!.map((e) => e.type.name.toCapitalize()).toList();
 
           // loop through to get the dominant color in an image
           PaletteGenerator palette = await PaletteGenerator.fromImageProvider(
@@ -87,7 +78,7 @@ class FetchPokemonBloc extends Bloc<FetchPokemonEvent, FetchPokemonState> {
 
           //calculate the average power and bmi
           var bmi = (e.weight! / (e.height! * e.height!)).toStringAsFixed(2);
-          var averagePower = double.parse(( statSum / 6).toStringAsFixed(2));
+          var averagePower = double.parse((statSum / 6).toStringAsFixed(2));
 
           var pokemon = Pokemon(
               types: typesList.join(','),
